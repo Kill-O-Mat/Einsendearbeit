6b65d14604fdbffe426f7a1312cdc8c4fea7c75f
mattae <mathias.taenzer@studium.fernuni-hagen.de>
2025-04-16 15:41:26 +0200

    e3.1

---
+++ src/e3_1/Planet.java
@@ -0,0 +1,23 @@
+package e3_1;
+
+public enum Planet {
+    JUPITER(30, 7785),
+    MARS(50, 2279);
+
+    private final int moons;
+    private final int entfernung;
+
+    Planet(int i, int x) {
+        this.moons = i;
+        this.entfernung = x;
+    }
+
+    public int getNumberOfMoons() {
+        return this.moons;
+    }
+
+    public boolean isCloserToSunThan(Planet planet) {
+
+        return (planet.entfernung > this.entfernung);
+    }
+}
